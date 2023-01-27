136d733c434d5f110dc46afee822d79cbdb26a2c
= <=>
2023-01-27 13:51:33 +0530

    gywe

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+<br/>
+<br/>
+<hr/>
+        <b>Click to Student and Find More Information</b>
+        <hr/>   
+        
+    </div>
+  )
+}
+
+export default Contact
